618d07286f2180de29cc0821f0aba464a1431742
BoySona <154109769+Boysona@users.noreply.github.com>
2025-05-15 14:35:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -442,30 +442,10 @@ def fallback(message):
         return send_subscription_message(message.chat.id)
     bot.send_message(message.chat.id, "⚠️ Please send only voice, audio, video, or a TikTok video link.")
 
-@app.route('/', methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        update = telebot.types.Update.de_json(request.get_data().decode('utf-8'))
-        bot.process_new_updates([update])
-        return '', 200
-    return abort(403)
-
-@app.route('/set_webhook', methods=['GET','POST'])
-def set_webhook():
-    url = "https://telegram-bot-media-transcriber.onrender.com"
-    bot.set_webhook(url=url)
-    return f"Webhook set to {url}", 200
-
-@app.route('/delete_webhook', methods=['GET','POST'])
-def delete_webhook():
-    bot.delete_webhook()
-    return 'Webhook deleted.', 200
-
 if __name__ == "__main__":
     if os.path.exists(DOWNLOAD_DIR):
         shutil.rmtree(DOWNLOAD_DIR)
     os.makedirs(DOWNLOAD_DIR, exist_ok=True)
     set_bot_info()
-    bot.delete_webhook()
-    bot.set_webhook(url="https://telegram-bot-media-transcriber.onrender.com")
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
+    bot.delete_webhook()  # Tani waa ok in la daayo si looga fogaado webhook.
+    bot.polling(none_stop=True)
